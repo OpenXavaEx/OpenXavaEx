c6e59a3ca6651acab358aefebaf1ea0d4167686f
thinkbase <thinkbase@lnspiron-7420>
2013-06-17 01:24:05 +0800

    Re-organize the build structure of OpenXava: make the app minimum file

---
+++ code/newProject.bat
@@ -0,0 +1,20 @@
+@echo off
+SETLOCAL
+pushd "%~dp0"
+
+::Check if java exists
+java -version
+IF %ERRORLEVEL% NEQ 0 (
+    echo "java" execution error: [%ERRORLEVEL%]
+    pause
+    exit /b %ERRORLEVEL%
+)
+
+::Run OpenXava ant command
+set PATH=%~dp0.\exe\apache-ant-1.9.1\bin;%PATH%
+pushd OpenXavaTemplate
+ant -buildfile CreateNewProject.xml
+popd
+
+popd
+ENDLOCAL
